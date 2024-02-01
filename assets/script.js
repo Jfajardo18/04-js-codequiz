6eb5de52ad1a400492b1623bc3677d9066c4ff85
Jonathanfajardo <jonnyfajardo18@gmail.com>
2024-02-01 18:40:58 -0500

    adding js variables

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+const startButton = document.getElementById('start-btn');
+const questionContainer = document.getElementById('question-container');
+const answerButtons = document.getElementById('answer-buttons');
+const resultContainer = document.getElementById('result-container');
+const resultText = document.getElementById('result-text');
+const timerContainer = document.getElementById('timer-container');
+const timerDisplay = document.getElementById('timer');
+const submitButton = document.getElementById('submit-btn');
+const initialsInput = document.getElementById('initials');
+
+
